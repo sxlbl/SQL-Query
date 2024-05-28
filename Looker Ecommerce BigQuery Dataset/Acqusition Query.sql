17493bfbb468fac6b000727b466c88445bb10800
박솔비 <154489441+sxlbl@users.noreply.github.com>
2024-05-28 18:05:35 +0900

    Update Acqusition Query.sql

--- Looker Ecommerce BigQuery Dataset/Acqusition Query.sql
+++ Looker Ecommerce BigQuery Dataset/Acqusition Query.sql
@@ -35,3 +35,27 @@ FROM (
       ON a.month = b.month
       )
 GROUP BY 1,2
+
+-- session_cnt, DT Trend
+WITH df as (
+            SELECT month
+              , session_id
+              , datetime_diff(end_session_dt, start_session_dt, MILLISECOND)/1000/60 as times_diff_minute
+            FROM (
+                  SELECT format_date("%Y-%m", date(datetime_trunc(created_at, month))) as month 
+                    , session_id
+                    , min(created_at) over (partition by session_id) as start_session_dt
+                    , max(created_at) over (partition by session_id) as end_session_dt
+                  FROM `sql-study-420204.looker_ecommerce_dataset.events` 
+                  )
+            -- 임의 세션유효시간
+            WHERE datetime_diff(end_session_dt, start_session_dt, MILLISECOND)/1000/60 <= 60
+             )
+
+
+SELECT month
+  , count(distinct session_id) as session_cnt
+  , avg(times_diff_minute) as avg_times_diff_minute
+FROM df
+GROUP BY 1
+ORDER BY 1
